a5839210085f4ead3125b4a5da8a5216b1abbfbb
Lucas Tinoco <lucas.tinoco@acanto.ca>
2023-04-24 14:16:57 -0300

    feat: users controller find by id and find all

---
+++ Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext : DbContext
+{
+    protected DataContext()
+    {
+    }
+
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<AppUser> Users { get; set; }
+}
